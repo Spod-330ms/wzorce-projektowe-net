3b0e6fceb46313b3dc12cc9488091a172b6537bb
Michal Stanislawek <stanislawek.michal@gmail.com>
2025-11-09 17:31:12 +0100

    Zadanie Prototype

---
+++ WzorceProjektoweNET/Solutions/EX04Prototype/Ork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX04Prototype
+{
+    public class Ork
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Strenght { get; set; }
+
+        public Ork(string name, int health, int strenght)
+        {
+            Name = name;
+            Health = health;
+            Strenght = strenght;
+        }
+        public Ork Clone()
+        {
+            return new Ork(Name, Health, Strenght);
+        }
+        public override string ToString()
+        {
+            return $"Ork: {Name}, Health: {Health}, Strenght: {Strenght}";
+        }
+    }
+}
---
+++ WzorceProjektoweNET/Solutions/EX04Prototype/Program.cs
@@ -0,0 +1,40 @@
+﻿using EX04Prototype;
+
+Ork ork1 = new Ork("Gorg", 100, 50);
+
+List<Ork> orksHorde = new List<Ork>();
+
+for (int i = 1; i <= 5; i++)
+{
+    Ork orkClone = ork1.Clone();
+    orkClone.Name = $"Gorg Clone {i}";
+    orkClone.Strenght = Random.Shared.Next(40, 101);
+
+    orksHorde.Add(orkClone);
+}
+
+Console.WriteLine("Horda sklonowanych Orków nadciąga!");
+Console.WriteLine(new string('-', 40));
+
+foreach (var ork in orksHorde)
+{
+    Console.WriteLine(ork);
+}
+
+Ork ork2 = new Ork("Borg", 140, 50);
+List<Ork> moreSerializedOrks = new List<Ork>();
+for (int i = 1; i <= 5; i++)
+{
+    string serializedOrk = Newtonsoft.Json.JsonConvert.SerializeObject(ork2);
+    Ork orkClone = Newtonsoft.Json.JsonConvert.DeserializeObject<Ork>(serializedOrk);
+    orkClone.Name = $"Gorg Serialized Clone {i}";
+    orkClone.Strenght = Random.Shared.Next(40, 101);
+    moreSerializedOrks.Add(orkClone);
+}
+
+Console.WriteLine("Kolejna horda sklonowanych Orków (serializacja) nadciąga!");
+Console.WriteLine(new string('-', 40));
+foreach (var ork in moreSerializedOrks)
+{
+    Console.WriteLine(ork);
+}
